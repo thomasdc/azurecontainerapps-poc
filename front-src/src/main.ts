58b6cdd35db74d4a54398c2d21864ee01f0c01f0
ThomasDC <1496708+thomasdc@users.noreply.github.com>
2022-11-16 21:47:48 +0100

    init quasar

--- front-src/src/main.ts
+++ front-src/src/main.ts
@@ -1,4 +1,13 @@
-import { createApp } from "vue";
+import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+
+// Import icon libraries
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
 import App from "./App.vue";
 import router from "./router";
 
@@ -7,5 +16,8 @@ import "./assets/main.css";
 const app = createApp(App);
 
 app.use(router);
+app.use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+  })
 
 app.mount("#app");
